6c03b15f5dc2412ab9a3b62c43fbfd3b0115b471
alexey-mr <alexey.morlang@gmail.com>
2020-09-16 13:08:20 +0300

    Dont reboot in scripts
    - reboot to be done in main orchestration logic

--- 03_undercloud_upgrade_rhel_step2.sh
+++ 03_undercloud_upgrade_rhel_step2.sh
@@ -1,18 +1,20 @@
-#!/bin/bash
-
-set -o xtrace
+#!/bin/bash -ex
 
 sudo dnf -y remove python2*
 
-echo =============== Upgrading undercloud director ==========================
-
 sudo subscription-manager release --set=8.2
 sudo subscription-manager repos --disable=*
-sudo subscription-manager repos --enable=rhel-8-for-x86_64-baseos-eus-rpms --enable=rhel-8-for-x86_64-appstream-eus-rpms --enable=rhel-8-for-x86_64-highavailability-eus-rpms --enable=ansible-2.9-for-rhel-8-x86_64-rpms --enable=openstack-16.1-for-rhel-8-x86_64-rpms --enable=fast-datapath-for-rhel-8-x86_64-rpms
+sudo subscription-manager repos \
+  --enable=rhel-8-for-x86_64-baseos-eus-rpms \
+  --enable=rhel-8-for-x86_64-appstream-eus-rpms \
+  --enable=rhel-8-for-x86_64-highavailability-eus-rpms \
+  --enable=ansible-2.9-for-rhel-8-x86_64-rpms \
+  --enable=openstack-16.1-for-rhel-8-x86_64-rpms \
+  --enable=fast-datapath-for-rhel-8-x86_64-rpms
+
 sudo dnf module disable -y container-tools:rhel8
 sudo dnf module enable -y container-tools:2.0
 
 sudo dnf distro-sync -y 
 
 echo "Perform reboot: sudo reboot"
-
