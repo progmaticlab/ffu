9097649a0506b37c043e636234362b4b6059dad0
alexey-mr <alexey.morlang@gmail.com>
2020-09-16 22:15:08 +0300

    Added enpoint env param to prepare
    - Fixed reboot of controller

--- 05_overcloud_prepare.sh
+++ 05_overcloud_prepare.sh
@@ -22,7 +22,7 @@ ansible-playbook -i ~/inventory.yaml $my_dir/playbook-ssh.yaml
 
 echo "Rebooting overclouds"
 ansible overcloud_Controller -i ~/inventory.yaml -b -m shell -a "pcs cluster stop"
-ansible-playbook  -i ~/inventory.yaml -l overcloud_ContrailController --forks=1 $my_dir/playbook-overcloud_node_reboot.yaml
+ansible-playbook  -i ~/inventory.yaml -l overcloud_Controller --forks=1 $my_dir/playbook-overcloud_node_reboot.yaml
 ansible overcloud_Controller -i ~/inventory.yaml -m ping
 ansible overcloud_Controller -i ~/inventory.yaml -b -m shell -a "pcs cluster start"
 ansible overcloud_Controller -i ~/inventory.yaml -b -m shell -a "pcs status"
--- 06_overcloud_upgrade.sh
+++ 06_overcloud_upgrade.sh
@@ -28,13 +28,15 @@ $my_dir/update_nic_templates.sh
 openstack overcloud upgrade prepare --templates tripleo-heat-templates/ \
   --stack overcloud --libvirt-type kvm \
   --roles-file tripleo-heat-templates/roles_data_contrail_aio.yaml \
-  -e tripleo-heat-templates/upgrades-environment.yaml \
+  -e docker_registry.yaml \
   -e tripleo-heat-templates/environments/rhsm.yaml \
-  -e tripleo-heat-templates/workaround.yaml \
+  -e rhsm.yaml \
   -e tripleo-heat-templates/environments/contrail/contrail-services.yaml \
   -e tripleo-heat-templates/environments/contrail/contrail-net-single.yaml \
+  -e tripleo-heat-templates/environments/contrail/endpoints-public-dns.yaml \
   -e tripleo-heat-templates/environments/contrail/contrail-plugins.yaml \
-  -e rhsm.yaml \
   -e misc_opts.yaml \
   -e contrail-parameters.yaml \
-  -e containers-prepare-parameter.yaml
+  -e containers-prepare-parameter.yaml \
+  -e tripleo-heat-templates/upgrades-environment.yaml \
+  -e tripleo-heat-templates/workaround.yaml
