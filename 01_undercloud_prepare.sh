98544bff7a8c300bc23d07cdfe13cecaa0c070a6
Gleb Galkin <gleb108@gmail.com>
2020-09-17 04:16:27 -0400

    bugfix

--- 01_undercloud_prepare.sh
+++ 01_undercloud_prepare.sh
@@ -3,10 +3,10 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname "$my_file")"
 
-ansible-playbook -c local -i localhost $my_dir/playbook-ssh.yaml
+ansible-playbook -c local -i localhost, $my_dir/playbook-ssh.yaml
 
-ansible-playbook -c local -i localhost $my_dir/playbook-nics.yaml
+ansible-playbook -c local -i localhost, $my_dir/playbook-nics.yaml
 
-ansible-playbook -c local -i localhost $my_dir/playbook-nics-vlans.yaml
+ansible-playbook -c local -i localhost, $my_dir/playbook-nics-vlans.yaml
 
 echo "Perform reboot: sudo reboot"
