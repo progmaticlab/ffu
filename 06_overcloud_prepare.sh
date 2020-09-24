0ecc2c08cb8094c7b3e6505b548ecf1e2dfffb9a
alexey-mr <alexey.morlang@gmail.com>
2020-09-24 18:09:19 +0300

    Optimize overcloud prepare and reboot

--- 06_overcloud_prepare.sh
+++ 06_overcloud_prepare.sh
@@ -27,16 +27,9 @@ ansible-playbook -i inventory.yaml $my_dir/playbook-nics-vlans.yaml
 ansible-playbook -i inventory.yaml -l overcloud_Compute $my_dir/playbook-nics-vhost0.yaml
 ansible-playbook -i inventory.yaml $my_dir/playbook-ssh.yaml
 
-
 echo "Rebooting overclouds"
 ansible overcloud_Controller -i inventory.yaml -b -m shell -a "pcs cluster stop"
-ansible-playbook  -i inventory.yaml -l overcloud_Controller --forks=1 $my_dir/playbook-overcloud_node_reboot.yaml
-ansible overcloud_Controller -i inventory.yaml -m ping
+ansible-playbook  -i inventory.yaml -l overcloud --forks=1 $my_dir/playbook-overcloud_node_reboot.yaml
+ansible overcloud -i inventory.yaml -m ping
 ansible overcloud_Controller -i inventory.yaml -b -m shell -a "pcs cluster start"
 ansible overcloud_Controller -i inventory.yaml -b -m shell -a "pcs status"
-
-ansible-playbook  -i inventory.yaml -l overcloud_ContrailController --forks=1 $my_dir/playbook-overcloud_node_reboot.yaml
-ansible overcloud_ContrailController -i inventory.yaml -m ping
-
-ansible-playbook  -i inventory.yaml -l overcloud_Compute $my_dir/playbook-overcloud_node_reboot.yaml
-ansible overcloud_Compute -i inventory.yaml -m ping
