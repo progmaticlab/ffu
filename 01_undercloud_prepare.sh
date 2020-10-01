99d51e962de3e3d5eab3fc69abe7863256c4cdff
Gleb Galkin <gleb108@gmail.com>
2020-10-01 12:55:48 +0300

    collecting information before update

--- 01_undercloud_prepare.sh
+++ 01_undercloud_prepare.sh
@@ -10,6 +10,24 @@ cd ~
 source stackrc
 source rhosp-environment.sh
 
+#Collect information about undercloud before upgrade
+uname -a >undercloud_uname.output
+rpm -qa > undercloud_installed_packages.txt
+cp /etc/os-release undercloud_os-release
+docker ps -a >undercloud_docker_ps.output
+
+#Collect overcloud node information before update
+node_admin_username=${NODE_ADMIN_USERNAME:-'heat-admin'}
+for node in overcloud-controller-0 overcloud-contrailcontroller-0 overcloud-novacompute-0; do
+    ip=$(openstack server list --name $node -c Networks -f value | cut -d '=' -f2)
+    [[ -n "$ip" ]]
+    ssh $node_admin_username@$ip "uname -a" >${node}_uname.output
+    ssh $node_admin_username@$ip "rpm -qa" >${node}_installed_packages.txt
+    ssh $node_admin_username@$ip "cat /etc/os-release" >${node}_os-release
+    ssh $node_admin_username@$ip "docker ps -a" >${node}_docker_ps.output
+done    
+
+
 ansible-playbook -c local -i localhost, $my_dir/playbook-ssh.yaml
 ansible-playbook -c local -i localhost, $my_dir/playbook-nics.yaml
 ansible-playbook -c local -i localhost, $my_dir/playbook-nics-vlans.yaml
